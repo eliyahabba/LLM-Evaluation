19f6c8da1f4beac40e37c91aa11fbcbea25781c4
eliyahabba <eliyahabba@gmail.com>
2024-02-28 13:57:31 +0200

    create QAModifier abstract class

--- src/Modifiers/QAModifier/QAModifier.py
+++ src/Modifiers/QAModifier/QAModifier.py
@@ -3,16 +3,18 @@ from pathlib import Path
 from src.Modifiers.DatasetModifier import DatasetModifier
 from src.utils.Constants import Constants
 
+from abc import abstractmethod
 DatasetModifierConstants = Constants.DatasetModifier
-SocialQAConstants = Constants.SocialQAModifier
 
 class QAModifier(DatasetModifier):
-    def __init__(self, dataset_path):
-        super().__init__(dataset_path)
-
-    def modify_dataset(self):
-        # Implement modifications specific to QA dataset
-        pass  # Implement QA modifications here
+    @abstractmethod
+    def _modify(self, data: str) -> str:
+        """
+        Modifies the dataset. Needs to be implemented by subclasses.
+        :param data: The dataset
+        :return: str: The modified dataset
+        """
+        raise NotImplementedError()
 
     def paraphrase_question(self, question, paraphraser_model):
         # Implement question paraphrasing
