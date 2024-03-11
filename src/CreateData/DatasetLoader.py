a177ab02f3080d18d613af51a20abeb0d72342de
eliyahabba <eliyahabba@gmail.com>
2024-03-11 18:38:14 +0200

    added new params

--- src/CreateData/DatasetLoader.py
+++ src/CreateData/DatasetLoader.py
@@ -5,11 +5,13 @@ from src.CreateData.LLMDataset import LLMDataset
 
 
 class DatasetLoader:
-    def __init__(self, card: str, template: Template, system_format: str,
+    def __init__(self, card: str, template: Template, system_format: str, num_demos: int, demos_pool_size: int,
                  max_instances: int, template_name: str):
         self.card = card
         self.template = template
         self.system_format = system_format
+        self.num_demos = num_demos
+        self.demos_pool_size = demos_pool_size
         self.max_instances = max_instances
         self.template_name = template_name
 
@@ -24,6 +26,8 @@ class DatasetLoader:
             # template="templates.qa.multiple_choice.with_context.no_intro.helm[enumerator=[option 1, option 2]]",
             template=self.template,
             format=self.system_format,
+            num_demos=self.num_demos,
+            demos_pool_size=self.demos_pool_size,
             max_train_instances=self.max_instances,
             max_validation_instances=self.max_instances,
             max_test_instances=self.max_instances,
