99c0d66a74da8ff28e200b035b2b1cfbc8f3cf27
eliyahabba <eliyahabba@gmail.com>
2024-03-11 17:39:49 +0200

    added max_instances

--- src/CreateData/DatasetLoader.py
+++ src/CreateData/DatasetLoader.py
@@ -5,14 +5,12 @@ from src.CreateData.LLMDataset import LLMDataset
 
 
 class DatasetLoader:
-    def __init__(self, card: str, template: Template, num_demos: int, demos_pool_size: int, system_format: str,
-                 max_train_instances: int, template_name: str):
+    def __init__(self, card: str, template: Template, system_format: str,
+                 max_instances: int, template_name: str):
         self.card = card
         self.template = template
-        self.num_demos = num_demos
-        self.demos_pool_size = demos_pool_size
         self.system_format = system_format
-        self.max_train_instances = max_train_instances
+        self.max_instances = max_instances
         self.template_name = template_name
 
     def load(self) -> LLMDataset:
@@ -23,11 +21,12 @@ class DatasetLoader:
         """
         recipe = StandardRecipe(
             card=self.card,
+            # template="templates.qa.multiple_choice.with_context.no_intro.helm[enumerator=[option 1, option 2]]",
             template=self.template,
-            num_demos=self.num_demos,
-            demos_pool_size=self.demos_pool_size,
             format=self.system_format,
-            max_train_instances=self.max_train_instances
+            max_train_instances=self.max_instances,
+            max_validation_instances=self.max_instances,
+            max_test_instances=self.max_instances,
         )
 
         dataset = recipe().to_dataset()
