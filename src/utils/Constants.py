338914de0b1af5b0106208385bf0db3059449d5b
eliyahabba <eliyahabba@gmail.com>
2024-03-11 10:43:39 +0200

    removed old params

--- src/utils/Constants.py
+++ src/utils/Constants.py
@@ -2,20 +2,6 @@ from pathlib import Path
 
 
 class Constants:
-    class DatasetModifierConstants:
-        DATA_PATH = Path(__file__).parents[2] / "Data"
-        MODIFIED_DATA_PATH = Path(__file__).parents[1] / "ModifiedData"
-        TEST_FILE = "test.json"
-
-    class SocialQAModifierConstants:
-        DATA_NAME = "SocialQA"
-
-    class WinograndeModifierConstants:
-        DATA_NAME = "Winogrande"
-
-    class MMLUModifierConstants:
-        DATA_NAME = "MMLU"
-
     class UnitxtDataConstants:
         DATA_PATH = 'unitxt/data'
         MULTIPLE_CHOICE_FOLDER_NAME = "MultipleChoiceTemplates"
