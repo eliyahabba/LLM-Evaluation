d334fef69a421005618a02bb39ba5a5fa7e74b62
eliyahabba <eliyahabba@gmail.com>
2024-03-11 17:40:05 +0200

    removed params

--- src/ModelsPredictors/LLMEvaluator.py
+++ src/ModelsPredictors/LLMEvaluator.py
@@ -109,12 +109,9 @@ class LLMEvaluator:
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--model_name", type=str, default="mistralai/Mistral-7B-Instruct-v0.2")
-    args.add_argument("--card", type=str, default="cards.ai2_arc.arc_challenge")
-    args.add_argument("--num_demos", type=int, default=5)
-    args.add_argument("--demos_pool_size", type=int, default=5)
+    args.add_argument("--card", type=str, default="cards.hellaswag")
     args.add_argument("--system_format", type=str, default="unitxt")
-    args.add_argument("--max_train_instances", type=int, default=5)
-
+    args.add_argument("--max_instances", type=int, default=5)
     args.add_argument("--template_name", type=str, default="template_0")
 
     args = args.parse_args()
@@ -125,8 +122,7 @@ if __name__ == "__main__":
 
     llm_dataset_loader = DatasetLoader(card=args.card,
                                        template=template,
-                                       num_demos=args.num_demos, demos_pool_size=args.demos_pool_size,
-                                       system_format=args.system_format, max_train_instances=args.max_train_instances,
+                                       system_format=args.system_format, max_instances=args.max_instances,
                                        template_name=args.template_name)
 
     llm_dataset = llm_dataset_loader.load()
