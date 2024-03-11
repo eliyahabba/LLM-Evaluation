07ddeb373cb39d7b7d4f86446478ac41ab48fda5
eliyahabba <eliyahabba@gmail.com>
2024-03-11 17:41:09 +0200

    fixed params

--- src/Experiments/run_experiment.py
+++ src/Experiments/run_experiment.py
@@ -12,12 +12,9 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--model_name", type=str, default="mistralai/Mistral-7B-Instruct-v0.2")
     args.add_argument("--card", type=str, default="cards.copa")
-    args.add_argument("--num_demos", type=int, default=1)
-    args.add_argument("--demos_pool_size", type=int, default=5)
     args.add_argument("--system_format", type=str, default="formats.user_agent")
-    args.add_argument("--max_train_instances", type=int, default=5)
+    args.add_argument("--max_instances", type=int, default=100)
     args.add_argument('--evaluate_on', nargs='+', default=['train'], help='The data types to evaluate the model on.')
-
     args.add_argument("--template_name", type=str, default="template_0")
 
     args = args.parse_args()
@@ -27,8 +24,7 @@ if __name__ == "__main__":
 
     llm_dataset_loader = DatasetLoader(card=args.card,
                                        template=template,
-                                       num_demos=args.num_demos, demos_pool_size=args.demos_pool_size,
-                                       system_format=args.system_format, max_train_instances=args.max_train_instances,
+                                       system_format=args.system_format, max_instances=args.max_instances,
                                        template_name=args.template_name)
 
     llm_dataset = llm_dataset_loader.load()
