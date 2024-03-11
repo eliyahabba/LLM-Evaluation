71e0908c5513273efba610233f664ed1d5a806d9
eliyahabba <eliyahabba@gmail.com>
2024-03-11 18:35:15 +0200

    added ExperimentConstants

--- src/utils/Constants.py
+++ src/utils/Constants.py
@@ -5,3 +5,7 @@ class Constants:
     class TemplatesGeneratorConstants:
         MULTIPLE_CHOICE_FOLDER_NAME = "MultipleChoiceTemplates"
         MULTIPLE_CHOICE_PATH = Path(__file__).parents[2] / "Data" / MULTIPLE_CHOICE_FOLDER_NAME
+
+    class ExperimentConstants:
+        RESULTS_FOLDER_NAME = "results"
+        RESULTS_PATH = Path(__file__).parents[2] / RESULTS_FOLDER_NAME
