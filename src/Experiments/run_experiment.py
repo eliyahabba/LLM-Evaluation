5e963074d6a94a25f08d42f8516565e016d702cc
eliyahabba <eliyahabba@gmail.com>
2024-03-11 18:39:46 +0200

    changed experiment params

--- src/Experiments/run_experiment.py
+++ src/Experiments/run_experiment.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 from src.CreateData.CatalogManager import CatalogManager
 from src.CreateData.DatasetLoader import DatasetLoader
@@ -7,15 +8,17 @@ from src.ModelsPredictors.LLMPredictor import LLMPredictor
 from src.utils.Constants import Constants
 
 TemplatesGeneratorConstants = Constants.TemplatesGeneratorConstants
-
+ExperimentConstants = Constants.ExperimentConstants
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--model_name", type=str, default="mistralai/Mistral-7B-Instruct-v0.2")
     args.add_argument("--card", type=str, default="cards.copa")
-    args.add_argument("--system_format", type=str, default="formats.user_agent")
+    args.add_argument("--system_format", type=str, default="formats.empty")
     args.add_argument("--max_instances", type=int, default=100)
     args.add_argument('--evaluate_on', nargs='+', default=['train'], help='The data types to evaluate the model on.')
     args.add_argument("--template_name", type=str, default="template_0")
+    args.add_argument("--num_demos", type=int, default=1)
+    args.add_argument("--demos_pool_size", type=int, default=10)
 
     args = args.parse_args()
 
@@ -24,11 +27,30 @@ if __name__ == "__main__":
 
     llm_dataset_loader = DatasetLoader(card=args.card,
                                        template=template,
-                                       system_format=args.system_format, max_instances=args.max_instances,
+                                       system_format=args.system_format,
+                                       num_demos=args.num_demos, demos_pool_size=args.demos_pool_size,
+                                       max_instances=args.max_instances,
                                        template_name=args.template_name)
 
     llm_dataset = llm_dataset_loader.load()
     llmp = LLMPredictor(args.model_name)
 
+    # save to json file the params of the experiment and the results of the evaluation
+    entry_experiment = {
+        "card": args.card,
+        "template_name": args.template_name,
+        "model_name": args.model_name,
+        "system_format": args.system_format,
+        "max_instances": args.max_instances,
+        "num_demos": args.num_demos,
+        "demos_pool_size": args.demos_pool_size,
+        "results": {}
+    }
+    json_file_name = "experiment_" + args.card + "_" + args.template_name + ".json"
+    results_path = ExperimentConstants.RESULTS_PATH
+    results_file_name = results_path / json_file_name
+    with open(results_file_name, 'w') as json_file:
+        json.dump(entry_experiment, json_file)
+
     llm_eval = LLMEvaluator(llmp)
-    results = llm_eval.predict_dataset(llm_dataset, args.evaluate_on)
+    results = llm_eval.predict_dataset(llm_dataset, args.evaluate_on, results_file_name=json_file_name)
