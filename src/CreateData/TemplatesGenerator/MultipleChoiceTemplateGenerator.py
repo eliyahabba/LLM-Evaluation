227776e631e82e977459e89748e03fb882f27282
eliyahabba <eliyahabba@gmail.com>
2024-03-12 15:04:45 +0200

    added datasets

--- src/CreateData/TemplatesGenerator/MultipleChoiceTemplateGenerator.py
+++ src/CreateData/TemplatesGenerator/MultipleChoiceTemplateGenerator.py
@@ -24,20 +24,55 @@ class MultipleChoiceTemplateGenerator(TemplateGenerator):
 
 if __name__ == "__main__":
     # Base arguments for all templates
-    base_args = {
+    dataset_name = "sciq"
+    base_args_copa = {
         "input_format": "The following are multiple choice questions (with answers)\n\nQuestion:"
                         " {question}\nChoose the correct answer from {numerals}\nAnswers:\n{choices}\nAnswer:",
         "choices_field": "choices",
         "target_field": "answer",
         "choices_seperator": "\n",
-        "enumerator": ["numbers", "capitals", "lowercase"],
+        "enumerator": "numbers",
         "postprocessors": ["processors.first_character"]
     }
+
+    base_args_sciq = {
+        "input_format": "Context: {context} Question: {question} Choices: {choices} Answer:",
+        "choices_field": "choices",
+        "target_field": "answer",
+        "choices_seperator": "\n",
+        "enumerator": "numbers",
+        "postprocessors": ["processors.first_character"]
+    }
+
+    base_args_race = {
+        "input_format": "Context: {context} Question: {question}. Answers: {choices}",
+        "choices_field": "choices",
+        "target_field": "answer",
+        "choices_seperator": "\n",
+        "enumerator": "numbers",
+        "postprocessors": ["processors.first_character"]
+    }
+
+    base_args_ai2_arc_easy = {
+        "input_format": "The following are multiple choice questions (with answers) about {topic}. Question: {question} Answers: {choices} Answer:",
+        "choices_field": "choices",
+        "target_field": "answer",
+        "choices_seperator": "\n",
+        "enumerator": "numbers",
+        "postprocessors": ["processors.first_character"]
+    }
+
+    dataset_name = "sciq"
+    base_args = base_args_sciq
+    dataset_name = "race"
+    base_args = base_args_race
+    dataset_name = "arc"
+    base_args = base_args_ai2_arc_easy
 
     # Override options for different parameters
     override_options = {
         "enumerator": ["capitals", "lowercase", "numbers", "roman"],
-        "choices_seperator": ["\n", ", ", "; ", " | ", "OR", "or"],
+        "choices_seperator": [" ", "\n", ", ", "; ", " | ", "OR", "or"],
         # Add more parameters and their possible values as needed
     }
 
@@ -46,6 +81,6 @@ if __name__ == "__main__":
     created_templates = generator.create_templates()
 
     # Save templates to local catalog
-    catalog_manager = CatalogManager(TemplatesGeneratorConstants.MULTIPLE_CHOICE_PATH)
+    catalog_manager = CatalogManager(TemplatesGeneratorConstants.MULTIPLE_CHOICE_PATH / dataset_name)
     for i, template in enumerate(created_templates):
         catalog_manager.save_to_catalog(template, f"template_{i}")
