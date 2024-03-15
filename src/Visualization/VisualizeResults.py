32c5c281c6bd681f084cfb62afc47cfefb444663
eliyahabba <eliyahabba@cs.huji.ac.il>
2024-03-15 09:27:14 +0200

    improved demo

--- src/Visualization/VisualizeResults.py
+++ src/Visualization/VisualizeResults.py
@@ -6,6 +6,8 @@ from typing import Tuple, List
 import pandas as pd
 import streamlit as st
 
+from src.CreateData.TemplatesGenerator.ConfigParams import ConfigParams
+
 file_path = Path(__file__).parents[2]
 sys.path.append(str(file_path))
 
@@ -21,18 +23,40 @@ class VisualizeResults:
     def display_page(self):
         st.title("Templates Visualization")
 
-        results_files = [f for f in RESULTS_FOLDER.iterdir() if f.is_dir()]
-        results_names_to_display = {f.name: f for f in results_files}
+        datasets_folders_names = [f for f in RESULTS_FOLDER.iterdir() if f.is_dir()]
+        datasets_names_to_display = {f.name: f for f in datasets_folders_names}
+        dataset_file_name = st.sidebar.selectbox("Select dataset to visualize", list(datasets_names_to_display.keys()))
+        selected_dataset_file_name = datasets_names_to_display[dataset_file_name]
 
-        card_file_name = st.sidebar.selectbox("Select dataset to visualize", list(results_names_to_display.keys()))
-        results_folder = results_names_to_display[card_file_name]
+        self.display_possible_templates_args()
+
+        shot_folders_name = [f for f in selected_dataset_file_name.iterdir() if f.is_dir()]
+        shot_folders_name = {f.name: f for f in shot_folders_name}
+        shot_file_name = st.sidebar.selectbox("Select the number of shots to visualize", list(shot_folders_name.keys()))
+        selected_shot_file_name = shot_folders_name[shot_file_name]
+
         # find the csv file in the folder if exists
-        result_file = [f for f in results_folder.iterdir() if f.is_file() and f.name.endswith(".csv")][0] if \
-            [f for f in results_folder.iterdir() if f.is_file() and f.name.endswith(".csv")] else None
+        result_files = [f for f in selected_shot_file_name.iterdir() if f.is_file() and f.name.endswith(".csv")]
+        if result_files:
+            train_file = [f for f in result_files if "train" in f.name]
+            assert len(train_file) >= 1, f"More than one train file found in the folder {selected_shot_file_name}"
+            test_file = [f for f in result_files if "test" in f.name]
+            assert len(test_file) >= 1, f"More than one test file found in the folder {selected_shot_file_name}"
+            result_files_to_display = {}
+            if len(train_file) == 1:
+                result_files_to_display['train_examples'] = train_file[0]
+            if len(test_file) == 1:
+                result_files_to_display['test_examples'] = test_file[0]
+            result_file_name = st.sidebar.selectbox("Select the results file to visualize", result_files_to_display)
+            result_file = result_files_to_display[result_file_name]
+        else:
+            st.markdown("No results file found in the folder")
+            st.stop()
+
         if result_file:
             results_file = result_file
             self.display_results(results_file)
-            self.display_sample_examples(results_folder)
+            self.display_sample_examples(selected_shot_file_name, dataset_file_name)
 
         else:
             st.markdown("No results file found in the folder")
@@ -45,6 +69,11 @@ class VisualizeResults:
         @return: None
         """
         df = pd.read_csv(results_file)
+        cols_to_remove = ['card', 'system_format', 'score', 'score_name']
+        columns_order = ['template_name', 'number_of_instances', 'accuracy', 'accuracy_ci_low', 'accuracy_ci_high',
+                         'score_ci_low', 'score_ci_high', 'groups_mean_score']
+        df = df.drop(cols_to_remove, axis=1)
+        df = df[columns_order]
         st.write(df)
 
     def load_results_preds(self, results_file: Path) -> Tuple[List[str], List[str]]:
@@ -59,36 +88,33 @@ class VisualizeResults:
         preds = [result['Result'] for result in results]
         return instances, preds
 
-    def display_sample_examples(self, results_folder: Path) -> None:
+    def display_sample_examples(self, results_folder: Path, dataset_file_name: str) -> None:
         """
         Display sample examples from the results file.
-        @param card_file_name: the name of the results file
+        @param results_folder: the path to the results folder
         @return: None
         """
         # select experiment file
-        results_names_to_display = {f.name.split("experiment_")[1].split('.json')[0]: f for f in
-                                    results_folder.iterdir() if
-                                    f.is_file() and f.name.endswith(".json")}
+        datasets_names_to_display = {f.name.split("experiment_")[1].split('.json')[0]: f for f in
+                                     results_folder.iterdir() if
+                                     f.is_file() and f.name.endswith(".json")}
         # sort the files by the number of the experiment
-        results_names_to_display = dict(
-            sorted(results_names_to_display.items(), key=lambda item: int(item[0].split("_")[1])))
-        results_file = st.sidebar.selectbox("Select template file", list(results_names_to_display.keys()))
-        instances, preds = self.load_results_preds(results_names_to_display[results_file])
+        datasets_names_to_display = dict(
+            sorted(datasets_names_to_display.items(), key=lambda item: int(item[0].split("_")[1])))
+        results_file = st.sidebar.selectbox("Select template file", list(datasets_names_to_display.keys()))
+        instances, preds = self.load_results_preds(datasets_names_to_display[results_file])
         st.write("Sample examples")
         for i in range(5):
             formatted_str = instances[i].replace("\n\n", "<br><br>").replace("\n", "<br>")
-            # st.markdown(f"Instance: {instances[i]}".replace("\n", "\n\n"))
             st.markdown(f"Instance: {formatted_str}", unsafe_allow_html=True)
-            # st.markdown(f"Instance: {instances[i]}")
             st.write(f"Prediction: {preds[i]}")
             st.write("----")
 
-        self.load_template(results_folder, results_file)
+        self.load_template(results_file, dataset_file_name)
 
-    def load_template(self, results_folder, results_file):
+    def load_template(self, results_file, dataset_file_name):
         templates_path = TemplatesGeneratorConstants.MULTIPLE_CHOICE_PATH
-        card = results_folder.name
-        template_path = templates_path / Path(card) / Path(f"{results_file}.json")
+        template_path = templates_path / dataset_file_name / Path(f"{results_file}.json")
         with open(template_path, "r") as f:
             template = json.load(f)
         # template is a dict, print each key value pair in the sidebar
@@ -101,6 +127,14 @@ class VisualizeResults:
                 st.sidebar.markdown(f"**{key}** : {value}")
             else:
                 st.sidebar.markdown(f"**{key}** : {value}")
+
+    def display_possible_templates_args(self):
+        with st.expander("Possible template arguments"):
+            override_options = ConfigParams.override_options
+            for k, v in override_options.items():
+                # display the options in the main page
+                st.markdown(f"The possible values for **{k}** are:  \n")
+                st.markdown(f"**{v}**")
 
 
 if __name__ == "__main__":
--- src/Visualization/streamlit_main.py
+++ src/Visualization/streamlit_main.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
                         help='add port to run the app. for example: --server_port 8501')
     args = parser.parse_args()
     
-    sys.argv = ["streamlit", "run", "src/Visualization/VisualizeResults.py",
+    sys.argv = ["streamlit", "run", "VisualizeResults.py",
                 # "--server.headless", "true",
                 # "global.developmentMode", "false",
                 ]
