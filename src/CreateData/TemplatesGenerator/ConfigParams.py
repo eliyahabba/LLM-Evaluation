a81a79978c964c31860ec9f6f92371fb55095551
eliyahabba <eliyahabba@gmail.com>
2024-03-27 12:17:22 +0200

    reformat file

--- src/CreateData/TemplatesGenerator/ConfigParams.py
+++ src/CreateData/TemplatesGenerator/ConfigParams.py
@@ -105,6 +105,6 @@ class ConfigParams:
     map_enumerator = {"ABCDEFGHIJKLMNOP": "capitals",
                       "abcdefghijklmnop": "lowercase",
                       str(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17',
-                       '18', '19', '20']): "numbers",
+                           '18', '19', '20']): "numbers",
                       str(['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV',
-                          'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX']): "roman"}
+                           'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX']): "roman"}
