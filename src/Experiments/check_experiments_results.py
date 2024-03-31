69c45b4001af7e75e069592a767c2792414372ac
eliyahabba <eliyahabba@gmail.com>
2024-03-31 11:04:57 +0300

    fixed check_experiments_results

--- src/Experiments/check_experiments_results.py
+++ src/Experiments/check_experiments_results.py
@@ -42,21 +42,25 @@ class ExperimentsResults:
 
 if __name__ == "__main__":
     # Load the model and the dataset
-    results_folder = ExperimentConstants.RESULTS_WITH_STRUCTURE_PATH
+    results_folder = ExperimentConstants.STRUCTURED_INPUT_FOLDER_PATH
     eval_on = ExperimentConstants.EVALUATE_ON
-
-    for dataset_folder in [file for file in results_folder.glob("*") if file.is_dir()]:
-        for shot_folder in [shot_folder for shot_folder in dataset_folder.glob("*") if shot_folder.is_dir()]:
-            files = list(shot_folder.glob("*.json"))
-            # Sort the files by the number in the name
-            sorted_file_paths = sorted(files, key=lambda x: int(x.name.split("_")[-1].split(".")[0]))
-            for results_file in sorted_file_paths:
-                try:
-                    experiments_results = ExperimentsResults(results_file, eval_on)
-                    results_counter = experiments_results.count_results()
-                    # if one of the values of results_counter < 100 then print the results_counter
-                    if any([value < 100 for value in results_counter.values()]):
-                        print(f"{results_file}: {results_counter}")
-                except Exception as e:
-                    print(f"Error in {results_file}: {e}")
-                    continue
+    models_names = [file for file in results_folder.glob("*") if file.is_dir()]
+    for model_name in models_names:
+        datasets = [file for file in model_name.glob("*") if file.is_dir()]
+        for dataset_folder in datasets:
+            shots = [file for file in dataset_folder.glob("*") if file.is_dir()]
+            for shot in shots:
+                formats = [file for file in shot.glob("*") if file.is_dir()]
+                for format_folder in formats:
+                    results_files = list([file for file in format_folder.glob("*.json")])
+                    sorted_file_paths = sorted(results_files, key=lambda x: int(x.name.split("_")[-1].split(".")[0]))
+                    for results_file in sorted_file_paths:
+                        try:
+                            experiments_results = ExperimentsResults(results_file, eval_on)
+                            results_counter = experiments_results.count_results()
+                            # if one of the values of results_counter < 100 then print the results_counter
+                            if any([value < 100 for value in results_counter.values()]):
+                                print(f"{results_file}: {results_counter}")
+                        except Exception as e:
+                            print(f"Error in {results_file}: {e}")
+                            continue
