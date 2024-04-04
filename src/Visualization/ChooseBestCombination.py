8f43a5bb5f957d260f3cf209d165b39e0363a683
eliyahabba <eliyahabba@cs.huji.ac.il>
2024-04-04 09:17:17 +0300

    added ChooseBestCombination

---
+++ src/Visualization/ChooseBestCombination.py
@@ -0,0 +1,131 @@
+import sys
+from pathlib import Path
+
+import pandas as pd
+import streamlit as st
+
+# file_path = Path(__file__).parents[2]
+# sys.path.append(str(file_path))
+
+from src.CreateData.TemplatesGenerator.ConfigParams import ConfigParams
+from src.utils.Constants import Constants
+
+TemplatesGeneratorConstants = Constants.TemplatesGeneratorConstants
+ExperimentConstants = Constants.ExperimentConstants
+
+
+class ChooseBestCombination:
+    def __init__(self, dataset_file_name: str, result_file: Path):
+        self.dataset_file_name = dataset_file_name
+        self.result_file = result_file
+        self.override_options = ConfigParams.override_options
+        if 'selected_best_value_axes' not in st.session_state:
+            st.session_state['selected_best_value_axes'] = list(self.override_options.keys())
+        if 'selected_average_value_axes' not in st.session_state:
+            st.session_state['selected_average_value_axes'] = []
+
+    def select_causal_factors(self):
+        """
+        Creates multiselect widgets for selecting axis options.
+
+        The user can select which axis options to use for getting the best value
+        and for getting the average value.
+        """
+
+        st.markdown("## Heatmap of the accuracy of the templates")
+        st.markdown("""
+        Select the axes to use for calculating the best value - We will recommend a specific value for each of these axes.  \nAlso, for the other axes (the complement of the selected axes), we calculate the average value of the parameters.
+        """)
+
+        # Multiselect for axis options to get the best value
+        self.select_best_value_axes()
+
+        # Multiselect for axis options to get the average value
+        self.select_average_value_axes()
+
+    def select_best_value_axes(self) -> None:
+        """
+        Allows the user to select axis options to get the best value.
+
+        Automatically updates the selection in case of changes.
+        """
+        st.multiselect("Select axes for finding the best value "
+                       "(we find the best value for these axes):",
+                       list(ConfigParams.override_options.keys()),
+                       key="selected_best_value_axes",
+                       on_change=self.update_average_value_axes)
+
+    def select_average_value_axes(self) -> None:
+        """
+        Allows the user to select axis options to get the average value.
+
+        Automatically updates the selection in case of changes.
+        """
+        st.multiselect(
+            "Select axes for average value (we calculate the average value for these axes"
+            " and not recommended on a specific value for these axes):",
+            list(ConfigParams.override_options.keys()),
+            key="selected_average_value_axes",
+            on_change=self.update_best_value_axes)
+
+    def update_average_value_axes(self) -> None:
+        """
+        Updates the session state variable for axis options without a value.
+
+        This function is triggered when the user selects/deselects options
+        in the multiselect for axis options to get the best value.
+        """
+        st.session_state['selected_average_value_axes'] = \
+            list(set(self.override_options.keys()) - set(st.session_state['selected_best_value_axes']))
+
+    def update_best_value_axes(self) -> None:
+        """
+        Updates the session state variable for axis options with a value.
+
+        This function is triggered when the user selects/deselects options
+        in the multiselect for axis options to get the average value.
+        """
+        st.session_state['selected_best_value_axes'] = \
+            list(set(self.override_options.keys()) - set(st.session_state['selected_average_value_axes']))
+
+    def choose_best_combination(self) -> pd.Series:
+        """
+        Choose the best combination of the values of the axes.
+        @return: The best combination of the values of the axes.
+        """
+        templates_path = TemplatesGeneratorConstants.MULTIPLE_CHOICE_PATH
+        metadata_file = templates_path / self.dataset_file_name / "templates_metadata.csv"
+        metadata_df = pd.read_csv(metadata_file, index_col='template_name')
+        self.select_causal_factors()
+        best_row = self.calculate_set_of_caused_factors(metadata_df)
+
+        st.markdown("#### The best set of caused factors:")
+        red_color = "#FF5733"
+        colors = ['blue', 'green', 'purple']
+        for i, option in enumerate(st.session_state['selected_best_value_axes']):
+            value = best_row[option]
+            if isinstance(value, str):
+                value = value.strip()
+            st.markdown(f'<span style="color:{colors[i % len(colors)]}">{option}: **{value}**</span>',
+                        unsafe_allow_html=True)
+        # take a red color for the accuracy
+        selected_average_value_axes_str = ', '.join(st.session_state['selected_average_value_axes'])
+        # take only the 2 digits after the point
+        accuracy = f"{best_row['accuracy']:.2f}"
+        acc_text = (f"These factors " +
+                    (f"(when we average the values of the other axes:"
+                     f" <strong>{selected_average_value_axes_str}</strong>) " if selected_average_value_axes_str else "") \
+                    + f"contribute to the **accuracy** <strong style='color:{red_color}'>{accuracy}</strong>")
+        st.markdown(acc_text, unsafe_allow_html=True)
+        return best_row
+
+    def calculate_set_of_caused_factors(self, metadata_df):
+        df = pd.read_csv(self.result_file)
+        # add the 'accuracy' columns from df to the metadata_df by the template_name
+        metadata_df = metadata_df.join(df.set_index('template_name')['accuracy'], on='template_name')
+        grouped_metadata_df = metadata_df.groupby(st.session_state['selected_best_value_axes'])[
+            'accuracy'].mean().rename(
+            'accuracy').reset_index()
+        # take the best row
+        best_row = grouped_metadata_df.loc[grouped_metadata_df['accuracy'].idxmax()]
+        return best_row
