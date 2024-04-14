836ab3885c0b190e347382e0c344dc7686f9afbc
eliyahabba <eliyahabba@cs.huji.ac.il>
2024-04-15 00:27:27 +0300

    plot only one theme

--- src/Visualization/PlotClustering.py
+++ src/Visualization/PlotClustering.py
@@ -44,15 +44,15 @@ class PlotClustering:
         Plot the clusters of the data
         @return: None
         """
-        fig = self.create_fig()
-        tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-        with tab1:
-            # Use the Streamlit theme.
-            # This is the default. So you can also omit the theme argument.
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-        with tab2:
-            # Use the native Plotly theme.
-            st.plotly_chart(fig, theme=None, use_container_width=True)
+        # fig = self.create_fig()
+        # tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+        # with tab1:
+        #     # Use the Streamlit theme.
+        #     # This is the default. So you can also omit the theme argument.
+        #     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        # with tab2:
+        #     # Use the native Plotly theme.
+        #     st.plotly_chart(fig, theme=None, use_container_width=True)
 
         fig = self.create_fig()
         st.plotly_chart(fig, theme=None, use_container_width=True)
