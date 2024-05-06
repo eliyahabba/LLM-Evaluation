ebbb130d2526b3144c3d09e5f0bc054f4b9501f0
eliyahabba <eliyahabba@gmail.com>
2024-05-06 16:26:49 +0300

    remove killable

--- src/sh_files/gemma/run_mmlu.sh
+++ src/sh_files/gemma/run_mmlu.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --mem=20g
+#SBATCH --mem=22g
 #SBATCH --time=2-0
-#SBATCH --gres=gpu:1,vmem:12g
+#SBATCH --gres=gpu:1,vmem:14g
 #SBATCH --mail-user=eliya.habba@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --exclude=cortex-03,cortex-04,cortex-05,cortex-06,cortex-07,cortex-08
-#SBATCH --killable
 
 
 export HF_HOME="/cs/snapless/gabis/gabis/shared/huggingface"
