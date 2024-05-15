d84f56ae43fcb45b1d75eee22ca07dbefa6b5a1b
eliyahabba <eliyahabba@gmail.com>
2024-05-15 10:49:30 +0300

    added LLAMA13B

---
+++ src/sh_files/llama13/run_mmlu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --mem=10g
+#SBATCH --time=3:0:0
+#SBATCH --gres=gpu:1,vmem:45g
+#SBATCH --mail-user=eliya.habba@mail.huji.ac.il
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+#SBATCH --exclude=cortex-03,cortex-04,cortex-05,cortex-06,cortex-07,cortex-08
+#SBATCH --killable
+
+export HF_HOME="/cs/snapless/gabis/gabis/shared/huggingface"
+export PYTHONPATH=/cs/labs/gabis/eliyahabba/LLM-Evaluation/
+
+sacct -j $SLURM_JOB_ID --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,nodelist
+module load cuda
+module load torch
+
+dir=/cs/labs/gabis/eliyahabba/LLM-Evaluation/src/Experiments/
+cd $dir
+
+source /cs/snapless/gabis/eliyahabba/venvs/LLM-Evaluation/bin/activate
+
+echo ${SLURM_ARRAY_TASK_ID}
+CUDA_LAUNCH_BLOCKING=1 python run_experiment.py --model_name LLAMA13B --card $1 --template_range $2 $3
