1d42e70593762d48be73f40d5838ae3c2899ea04
eliyahabba <eliyahabba@cs.huji.ac.il>
2024-05-24 10:21:46 +0300

    sh files

--- src/sh_files/phi/run_mmlu_small.sh
+++ src/sh_files/phi/run_mmlu_small.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem=20g
 #SBATCH --time=2-0
-#SBATCH --gres=gpu:1,vmem:8g
+#SBATCH --gres=gpu:1,vmem:10g
 #SBATCH --mail-user=eliya.habba@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --exclude=cortex-01,cortex-02,cortex-03,cortex-04,cortex-05,cortex-06,cortex-07,cortex-08
@@ -20,4 +20,4 @@ cd $dir
 source /cs/snapless/gabis/eliyahabba/venvs/LLM-Evaluation/bin/activate
 
 echo ${SLURM_ARRAY_TASK_ID}
-CUDA_LAUNCH_BLOCKING=1 python run_experiment.py --model_name PHI_SMALL --card $1 --template_range $2 $3 --trust_remote_code
+CUDA_LAUNCH_BLOCKING=1 python run_experiment.py --model_name PHI_SMALL --card $1 --template_range $2 $3 --trust_remote_code --not_load_in_8bit
