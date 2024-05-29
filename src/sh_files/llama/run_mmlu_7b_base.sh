598e1b123798b72b16b193e3e9ee57e85d5b9db0
eliyahabba <eliyahabba@gmail.com>
2024-05-29 15:43:02 +0300

    sh files

--- src/sh_files/llama/run_mmlu_7b_base.sh
+++ src/sh_files/llama/run_mmlu_7b_base.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --mem=20g
-#SBATCH --time=1-0
-#SBATCH --gres=gpu:1,vmem:12g
+#SBATCH --mem=10g
+#SBATCH --time=6:0:0
+#SBATCH --gres=gpu:1,vmem:10g
 #SBATCH --mail-user=eliya.habba@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --exclude=cortex-03,cortex-04,cortex-05,cortex-06,cortex-07,cortex-08
-
+#SBATCH --killable
 
 export HF_HOME="/cs/snapless/gabis/gabis/shared/huggingface"
 export PYTHONPATH=/cs/labs/gabis/eliyahabba/LLM-Evaluation/
@@ -20,4 +20,4 @@ cd $dir
 source /cs/snapless/gabis/eliyahabba/venvs/LLM-Evaluation/bin/activate
 
 echo ${SLURM_ARRAY_TASK_ID}
-CUDA_LAUNCH_BLOCKING=1 python run_experiment.py  --model_name LLAMA --card cards.hellaswag --template_range $1 $2
+CUDA_LAUNCH_BLOCKING=1 python run_experiment.py --model_name LLAMA7B_BASE --card $1 --template_range $2 $3
