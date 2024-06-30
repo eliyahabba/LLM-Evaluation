b5744f71a17c09b658227366d4acc93e62cc2529
eliyahabba <eliyahabba@gmail.com>
2024-06-30 18:59:57 +0300

    save  data

--- src/Experiments/count_data.py
+++ src/Experiments/count_data.py
@@ -33,7 +33,11 @@ class ExperimentsResultsFolder:
         Count the results of the model on the dataset.
         @return:
         """
-        self.load_experiment_file(results_file)
+        try:
+            self.load_experiment_file(results_file)
+        except Exception as e:
+            print(f"Error loading file {results_file}: {e}")
+            return 0
         results = self.experiment['results']
         if self.eval_on not in results:
             return 0
@@ -53,24 +57,26 @@ class ExperimentsResultsFolder:
 
         all_files_names = [f"experiment_template_{i}" for i in range(0, 56)]
         sorted_file_names = [file.name.split(".json")[0] for file in sorted_file_paths]
-        
+
         missing_files = [file for file in all_files_names if file not in sorted_file_names]
-        files_percentage = {}
-        files_completed = {}
+        files_total_data = {}
+        files_acquired_data = {}
         for missing_file in missing_files:
             configuration_number = int(missing_file.split("experiment_template_")[1])
-            files_percentage[configuration_number] = 0
-            files_completed[configuration_number] = False
-            
+            total_data = dataset_size.iloc[0].to_dict()[self.eval_on]
+            data_acquired = 0
+            files_total_data[configuration_number] = total_data
+            files_acquired_data[configuration_number] = data_acquired
+
         for results_file in sorted_file_paths:
             results_counter = experiments_results.count_results(results_file)
             # if one of the values of results_counter < 100 then print the results_counter
             configuration_number = int(results_file.name.split("experiment_template_")[1].split(".")[0])
-            percentage_of_evaluated_data = results_counter // dataset_size.iloc[0].to_dict()[self.eval_on]
-            files_percentage[configuration_number] = percentage_of_evaluated_data    
-            files_completed[configuration_number] = percentage_of_evaluated_data == 100
+            # percentage_of_evaluated_data = (results_counter // dataset_size.iloc[0].to_dict()[self.eval_on]) * 100
+            files_total_data[configuration_number] = dataset_size.iloc[0].to_dict()[self.eval_on]
+            files_acquired_data[configuration_number] = results_counter
+        return files_total_data, files_acquired_data
 
-        return files_percentage, files_completed
 
 def check_comparison_matrix(format_folder: Path, eval_value: str, kwargs: dict = None):
     if not format_folder.exists():
@@ -96,14 +102,15 @@ def check_comparison_matrix(format_folder: Path, eval_value: str, kwargs: dict =
 def create_summarize_df():
     names = ['Model', 'Dataset', 'Shots', "Configuration"]
     index = pd.MultiIndex(levels=[[], [], [], []], codes=[[], [], [], []], names=names)
-    df = pd.DataFrame(index=index, columns=['Data Percentage', 'Data Completed'])
+    df = pd.DataFrame(index=index, columns=['Total Data', 'Data Acquired'])
     # add a dummmy row to the df with the update_function
-    df.loc[('dummy', 'dummy', 'dummy', "dummy"), ['Data Percentage', 'Data Completed']] = [0, False]
+    df.loc[('dummy', 'dummy', 'dummy', "dummy"), ['Total Data', 'Data Acquired']] = [0, False]
     return df
 
 
 def create_save_summarize_df(file_path):
     df = create_summarize_df()
+    # df.index = df.index.set_levels(df.index.levels[3].astype(int), level=3)
     df.to_csv(file_path)
 
 
@@ -113,18 +120,20 @@ def get_summarize_df(file_path):
     return pd.read_csv(file_path, index_col=[0, 1, 2, 3])
 
 
-def udpate_summarize_df(df, model, dataset, shots, config_number, data_percentage, data_completed):
+def udpate_summarize_df(df, model, dataset, shots, config_number, total_data, data_acquired):
     df.sort_index(inplace=True)
-    if (model, dataset, shots) not in df.index:
+    if (model, dataset, shots, config_number) not in df.index:
         # add the new row
         new_row = pd.DataFrame({
-            'Data Percentage': [data_percentage],
-            'Data Completed': [data_completed]
+            'Total Data': [total_data],
+            'Data Acquired': [data_acquired],
         }, index=pd.MultiIndex.from_tuples([(model, dataset, shots, config_number)],
                                            names=['Model', 'Dataset', 'Shots', "Configuration"]))
         df = pd.concat([df, new_row])
     else:
-        df.loc[(model, dataset, shots), ['Data Percentage', 'Data Completed']] = [data_percentage, data_completed]
+        df.loc[(model, dataset, shots, config_number), ['Total Data', 'Data Acquired']] = [
+            total_data, data_acquired]
+
     return df
 
 
@@ -132,11 +141,12 @@ def save_updated_summarize_df(df, file_path):
     df.to_csv(file_path)
 
 
-def update_df_files(files_percentage, files_completed, df, model_name, mmlu_dataset, shots):
-    for config_number, data_percentage in files_percentage.items():
-        data_completed = files_completed[config_number]
-        df = udpate_summarize_df(df, model_name, mmlu_dataset, shots,config_number, data_percentage, data_completed)
-    return df        
+def update_df_files(files_total_data, files_acquired_data, df, model_name, mmlu_dataset, shots):
+    for config_number, total_data in files_total_data.items():
+        data_acquired = files_acquired_data[config_number]
+        df = udpate_summarize_df(df, model_name, mmlu_dataset, shots, config_number,
+                                 total_data, data_acquired)
+    return df
 
 
 if __name__ == "__main__":
@@ -152,11 +162,11 @@ if __name__ == "__main__":
     df = get_summarize_df(ResultConstants.SUMMARIZE_DF_PATH)
     MMLUData.initialize()
     # run the function on all the models and datasets
-    for model in tqdm(LLMProcessorConstants.MODEL_NAMES.values()):
+    for model in tqdm(list(LLMProcessorConstants.MODEL_NAMES.values())[15:]):
         model_name = model.split("/")[-1]
-        for mmlu_dataset in MMLUData.get_mmlu_datasets():
+        for mmlu_dataset in tqdm(list(MMLUData.get_mmlu_datasets())):
             for shots in ResultConstants.SHOTS:
-                files_percentage, files_completed = experiments_results. \
+                files_total_data, files_acquired_data = experiments_results. \
                     get_data_percentage_and_completed(results_folder, model_name, mmlu_dataset, shots)
-                df = update_df_files(files_percentage, files_completed, df, model_name, mmlu_dataset, shots)
-    save_updated_summarize_df(df, ResultConstants.SUMMARIZE_DF_PATH)
+                df = update_df_files(files_total_data, files_acquired_data, df, model_name, mmlu_dataset, shots)
+                save_updated_summarize_df(df, ResultConstants.SUMMARIZE_DF_PATH)
