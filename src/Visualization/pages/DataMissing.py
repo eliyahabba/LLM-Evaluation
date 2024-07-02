bb4c7ac5f6ee179aca51dc00acb21e5212a31ded
eliyahabba <eliyahabba@gmail.com>
2024-07-02 17:01:20 +0300

    added page

--- src/Visualization/pages/DataMissing.py
+++ src/Visualization/pages/DataMissing.py
@@ -19,10 +19,31 @@ class DataMissing:
         # columns: Total Data,Data Acquired
         # sum the number of Data Acquired (this is a int column)
         sum_of_data_acquired = self.df["Data Acquired"].sum()
+        # this is a bit number so we need to format it with commas
+        sum_of_data_acquired = "{:,}".format(sum_of_data_acquired)
         st.markdown(f"Total number of data acquired: {sum_of_data_acquired}")
 
+    def count_missing_data_by_model_and_shots(self):
+        # count the number of missing data by model and shots (Data Acquired % Total Data)
+        # Data_Acquired = self.df.groupby(["Model", "Shots"])["Data Acquired"].sum()
+        # Total_Data = self.df.groupby(["Model", "Shots"])["Total Data"].sum()
+        # group_df = self.df.groupby(["Model", "Shots"]).size().reset_index(name='counts')
+        # group_df["Data Acquired % Total Data"] = Data_Acquired / Total_Data
+        # group_df["Data Acquired % Total Data"] = group_df["Data Acquired % Total Data"].apply(lambda x: round(x, 2))
 
+        Data_Acquired = self.df.groupby(["Model", "Shots"])["Data Acquired"].sum()
+        Total_Data = self.df.groupby(["Model", "Shots"])["Total Data"].sum()
 
+        group_df = pd.DataFrame({
+            "Data Acquired": Data_Acquired,
+            "Total Data": Total_Data
+        }).reset_index()
+
+        group_df["Data Acquired % Total Data"] = (group_df["Data Acquired"] / group_df["Total Data"]).apply(lambda x: round(x, 2)) * 100
+        # sort the df by the Data Acquired % Total Data
+        group_df = group_df.sort_values(by="Data Acquired % Total Data", ascending=False, ignore_index=True)
+        # reset the index
+        st.write(group_df)
 
     def filter_by_model(self):
         # the user need to select the model from the list of models
@@ -37,13 +58,13 @@ class DataMissing:
         st.markdown(f"Model: {model}, Shots: {shots}")
         st.write(curr_df)
         # take only the uncompleted data
-        uncompleted_data = curr_df[curr_df["Data Completed"] == False]
+        uncompleted_data = curr_df[curr_df["Data Acquired"] != curr_df["Total Data"]]
         st.markdown("Uncompleted data")
         st.write(uncompleted_data)
-
 
 
 if __name__ == "__main__":
     data = DataMissing(ResultConstants.SUMMARIZE_DF_PATH)
     data.read_data()
+    data.count_missing_data_by_model_and_shots()
     data.filter_by_model()
