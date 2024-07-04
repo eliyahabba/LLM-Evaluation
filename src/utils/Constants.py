a3d013315ccf3124259de2364dba994e6311242e
eliyahabba <eliyahabba@gmail.com>
2024-07-04 13:19:45 +0300

    PREDICT_PROB_OF_TOKENS = False

--- src/utils/Constants.py
+++ src/utils/Constants.py
@@ -85,7 +85,7 @@ class Constants:
         LOAD_IN_8BIT = True
         TRUST_REMOTE_CODE = False
         RETURN_TOKEN_TYPE_IDS = None
-        PREDICT_PROB_OF_TOKENS = True
+        PREDICT_PROB_OF_TOKENS = False
 
         MAX_NEW_TOKENS = 10
 
