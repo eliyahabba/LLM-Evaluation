c6fc41aa2106aa16fda324e673fb74dc86022c59
Leshem Choshen <borgr@users.noreply.github.com>
2024-07-15 09:59:22 -0400

    Update load_config.sh
    Add conda variable

--- src/sh_files/load_config.sh
+++ src/sh_files/load_config.sh
@@ -35,13 +35,34 @@ echo "Using Hugging Face home directory at $HF_HOME"
 export HF_HOME=$HF_HOME
 
 VENV=$(python $config_path venv)
+CONDA=$(python $config_path conda)
 
-# Check if VENV is empty
-if [ -z "$VENV" ]; then
-    echo "Failed to load VENV configuration."
+# Check if VENV is defined
+if [ -n "$VENV" ]; then
+    # VENV is defined, so we use it
+    absolute_path=$(readlink -f "$VENV")
+    echo "Using VENV at $absolute_path"
+    if [ -f "$absolute_path" ]; then
+        source "$absolute_path"
+        echo "Virtual environment activated."
+    else
+        echo "Error: VENV file not found at $absolute_path"
+        exit 1
+    fi
+# If VENV is not defined, check if CONDA is defined
+elif [ -n "$CONDA" ]; then
+    echo "Using Conda environment"
+    # Ensure conda command is available
+    if command -v conda >/dev/null 2>&1; then
+        # shellcheck disable=SC1091
+        source "$(conda info --base)/etc/profile.d/conda.sh"
+        conda activate "$CONDA"
+        echo "Conda environment '$CONDA' activated."
+    else
+        echo "Error: conda command not found"
+        exit 1
+    fi
+else
+    echo "Error: Neither VENV nor CONDA variable is defined."
     exit 1
 fi
-
-absolute_path=$(readlink -f $VENV)
-echo "Using VENV at $absolute_path"
-source $VENV
