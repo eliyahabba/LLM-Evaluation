554ab9d24174367e8892d2bd5095a3ec5b020b07
eliyahabba <eliyahabba@cs.huji.ac.il>
2024-07-16 22:57:10 +0300

    calculate_perplexity

--- src/sh_files/general/calculate_perplexity.sh
+++ src/sh_files/general/calculate_perplexity.sh
@@ -14,7 +14,7 @@
 
 
 load_config_path="load_config.sh"
-config_bash=$(readlink -f load_config_path)
+config_bash=$(readlink -f $load_config_path)
 echo "Loading config with: " $config_bash
 source $config_bash
 
