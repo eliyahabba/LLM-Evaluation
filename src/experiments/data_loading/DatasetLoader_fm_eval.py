1c795a678c93ea2fa4df381c1e3af1126b443f6f
ehabba <ehabba@lightricks.com>
2024-09-25 08:45:36 +0300

    added fm eval

---
+++ src/experiments/data_loading/DatasetLoader_fm_eval.py
@@ -0,0 +1,123 @@
+import os
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from unitxt.formats import SystemFormat
+from unitxt.standard import StandardRecipe
+from unitxt.templates import Template, MultipleChoiceTemplate
+from src.experiments.data_loading.NLPDataset import NLPDataset
+from src.utils.Constants import Constants
+
+TemplatesGeneratorConstants = Constants.TemplatesGeneratorConstants
+
+
+@dataclass
+class _DefaultUnitxtRecipeArgs(UnitxtRecipeArgs):
+    card: str
+    template: Template
+    format: List[str] = field(default_factory=lambda: ["formats.empty"])
+    demos_taken_from: str = "train"
+    num_demos: int = 0
+    demos_pool_size: int = 100
+    max_train_instances: int = 1000
+    max_validation_instances: int = 1000
+    max_test_instances: int = 1000
+    system_prompt: List[str] = field(default_factory=lambda: ["system_prompts.empty"])
+
+
+class DatasetLoader:
+    def __init__(self, card: str, template: Template, system_format: str, demos_taken_from: str, num_demos: int,
+                 demos_pool_size: int,
+                 max_instances: int, template_name: str):
+        self.card = card
+        self.template = template
+        self.system_format = system_format
+        self.demos_taken_from = demos_taken_from
+        self.num_demos = num_demos
+        self.demos_pool_size = demos_pool_size
+        self.max_instances = max_instances
+        self.template_name = template_name
+
+    def read_dataset_sizes(self):
+        """
+        Reads the MMLU dataset sizes from the file.
+
+        @return: The MMLU dataset sizes
+        """
+        dataset_sizes = pd.read_csv(TemplatesGeneratorConstants.DATASET_SIZES_PATH)
+        return dataset_sizes
+
+    def get_data_size(self, card: str, data_type:str):
+        """
+        Gets the validation size for the specified card.
+
+        @param card: The card
+        @return: The validation size
+        """
+        dataset_sizes = self.read_dataset_sizes()
+        validation_size = \
+            dataset_sizes[dataset_sizes["Name"] == card.split("cards.")[1]][data_type].values[0]
+        return validation_size
+
+    def load(self) -> _DefaultUnitxtRecipeArgs:
+        """
+        Loads the dataset from the specified path.
+
+        @return: The dataset
+        """
+        if self.system_format.startswith("formats."):
+            system_format = self.system_format
+        else:
+            system_format = SystemFormat(
+                model_input_format=f"{self.system_format}\n{{source}}",
+            )
+        if self.num_demos:
+            validation_size = self.get_data_size(self.card, data_type="validation")
+            if not np.isnan(validation_size):
+                self.demos_pool_size = min(validation_size - 1, self.demos_pool_size)
+            else:
+                self.demos_taken_from = 'train'
+                train_size = self.get_data_size(self.card,data_type="train") - 1
+                self.demos_pool_size = min(train_size, self.demos_pool_size)
+
+
+        unitxt_recipe_args = _DefaultUnitxtRecipeArgs(
+            card=self.card,
+            template=self.template,
+            format=system_format,
+            demos_taken_from=self.demos_taken_from,
+            num_demos=self.num_demos,
+            demos_pool_size=self.demos_pool_size,
+            max_train_instances=self.max_instances,
+            max_validation_instances=self.max_instances,
+            max_test_instances=self.max_instances,
+            system_prompt=["system_prompts.empty"]
+        )
+        return unitxt_recipe_args
+
+
+
+if __name__ == '__main__':
+    os.environ["UNITXT_ALLOW_UNVERIFIED_CODE"] = "True"
+    topic = "Topic_1"
+    question = "What is the capital of France?"
+    choices = ["Paris, London, Berlin, Rome"]
+
+    input_format=(f"Question: [question] Choices: [choices] Answer:"
+                  f" [Answer]\nChoices: {choices} Answer:)")
+    # input_format=(f"Question")
+    template = MultipleChoiceTemplate(input_format=input_format,
+    target_prefix="",
+    choices_field= "choices",
+    target_field = "answer",
+    choices_separator= ", ",
+    source_choice_format = "{choice_numeral}. {choice_text}",
+    target_choice_format = "{choice_numeral}",
+    enumerator = "capitals",
+    shuffle_choices= False
+    )
+    loader = DatasetLoader("cards.mmlu_pro.biology", template, "empty_system_format",
+                           "train", 1, 2, 1000, "mmlu")
+    unitxt_recipe_args = loader.load()
+
