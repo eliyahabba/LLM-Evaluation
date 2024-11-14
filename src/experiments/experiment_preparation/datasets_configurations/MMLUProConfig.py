3d1d273496c1ac461300edd42a994feec974345e
ehabba <ehabba@lightricks.com>
2024-11-14 17:33:27 +0200

    fixed prompt

--- src/experiments/experiment_preparation/datasets_configurations/MMLUProConfig.py
+++ src/experiments/experiment_preparation/datasets_configurations/MMLUProConfig.py
@@ -17,9 +17,12 @@ class MMLUConfig(BaseDatasetConfig):
     def get_mmlu_instructions_without_topic_helm(self) -> str:
         return f"The following are multiple choice questions (with answers).\n\nQuestion: {{question}}\n\n{{choices}}\nAnswer:"
 
+    def get_mmlu_instructions_without_topic_lm_evaluation_harness(self) -> str:
+        return f"Question: {{question}}\n\nChoices: {{choices}}\nAnswer:"
+    
     def get_mmlu_instructions_with_topic_and_cot(self):
         return (f"The following are multiple choice questions (with answers) about {{topic}}. Think step by"
-                  f" step and then output the answer in the format of \"The answer is (X)\" at the end.\n\n")
+                f" step and then output the answer in the format of \"The answer is (X)\" at the end.\n\n")
 
     def get_structured_instruction_text_with_topic(self):
         # Provide a default implementation if needed
@@ -28,6 +31,7 @@ class MMLUConfig(BaseDatasetConfig):
     def get_structured_instruction_text_without_topic(self):
         # Provide a default implementation if needed
         return f"Question: [question] Choices: [choices] Answer: [answer]\nQuestion: {{question}} Choices: {{choices}} Answer:"
+
 
 if __name__ == "__main__":
     config = MMLUConfig({"shuffle_choices": True})
