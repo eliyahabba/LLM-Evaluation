39682ed49c2d8a5202b05b1c7aaa83488e95258a
ehabba <ehabba@lightricks.com>
2024-11-24 18:55:53 +0200

    changed data

--- src/experiments/experiment_preparation/configuration_generation/generate_templates.py
+++ src/experiments/experiment_preparation/configuration_generation/generate_templates.py
@@ -2,7 +2,7 @@ import argparse
 import itertools
 import os
 from enum import Enum
-from typing import Dict, List
+from typing import Dict, List, Any
 
 
 class BaseConfig:
@@ -78,16 +78,90 @@ class BaseConfig:
             return [shot.value for shot in cls]
 
     class PromptOptions:
-        """Prompt variation options"""
-        GREEK_CHARS = "αβγδεζηθικ"
+        GREEK_CHARS = "αβγδεζηθικ"  # 10 Greek letters
+        KEYBOARD_CHARS = "!@#$%^₪*)("  # 26 lowercase letters
+        override_options = {
+            "enumerator": ["capitals", "lowercase", "numbers", "roman", KEYBOARD_CHARS, GREEK_CHARS],
+
+            "choices_separator": [" ", "\n", ", ", "; ", " | ", " OR ", " or "],
+            "shuffle_choices": [False, True],
+            # Add more parameters and their possible values as needed
+        }
+
+        ENUM_CHARS = {"ABCDEFGHIJKLMNOP": "capitals",
+                      "abcdefghijklmnop": "lowercase",
+                      str(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17',
+                           '18', '19', '20']): "numbers",
+                      str(['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV',
+                           'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX']): "roman",
+                      KEYBOARD_CHARS: "keyboard",  # Added mapping for keyboard chars
+                      GREEK_CHARS: "greek"  # Added mapping for greek chars
+                      }
 
         @classmethod
         def get_options(cls) -> Dict[str, List]:
+            return cls.override_options
+
+        @classmethod  # Changed to classmethod to access class attributes
+        def format_value(cls, value: Any) -> str:
+            """Format a value for use in template name"""
+            if isinstance(value, bool):
+                return str(value)
+            elif isinstance(value, str):
+                # Special characters mapping
+                separator_names = {
+                    " ": "space",
+                    "\n": "newline",
+                    " | ": "pipe",
+                    " OR ": "OrCapital",
+                    " or ": "orLower",
+                    ", ": "comma",
+                    "; ": "semicolon"
+                }
+
+                # Check separators first
+                if value in separator_names:
+                    return separator_names[value]
+
+                # Check special character sets
+                if value in cls.ENUM_CHARS:
+                    return cls.ENUM_CHARS[value]
+
+                # For any other string values
+                return value.replace(" ", "")
+
+            return str(value)
+
+        @classmethod
+        def generate_template_name(cls, combination: Dict[str, Any]) -> str:
+            """Generate a descriptive template name from a combination of options"""
+            parts = []
+            for key, value in combination.items():
+                formatted_value = cls.format_value(value)
+                camel_key = cls.to_camel_case(key)
+                parts.append(f"{camel_key}_{formatted_value}")
+            return "_".join(parts)
+
+        @staticmethod
+        def to_camel_case(snake_str: str) -> str:
+            """Convert snake_case to camelCase"""
+            components = snake_str.split('_')
+            return components[0] + ''.join(x.title() for x in components[1:])
+
+        @classmethod
+        def generate_template_combinations(cls) -> Dict[str, Dict]:
+            """Generate all possible combinations of template options"""
+            options = cls.get_options()
+            combinations = [
+                {key: value for key, value in zip(options.keys(), values)}
+                for values in itertools.product(*options.values())
+            ]
+
             return {
-                "enumerator": ["capitals", "lowercase", "numbers", "roman", "!@#$%^₪*)(", cls.GREEK_CHARS],
-                "choices_separator": [" ", "\n", ", ", "; ", " | ", " OR ", " or "],
-                "shuffle_choices": [False, True],
+                cls.generate_template_name(combo): combo
+                for combo in combinations
             }
+
 
     class ModelConfigs:
         """Model configurations"""
@@ -127,23 +201,14 @@ class BaseConfig:
                 for model_info in configs[family].values()
             ]
 
-    @classmethod
-    def generate_template_combinations(cls) -> List[Dict]:
-        """Generate all possible combinations of template options"""
-        options = cls.PromptOptions.get_options()
-        return [
-            {key: value for key, value in zip(options.keys(), values)}
-            for values in itertools.product(*options.values())
-        ]
-
 
 def run_experiment(local_catalog_path: str):
     """Run the experiment with given configuration"""
     os.environ["UNITXT_ARTIFACTORIES"] = local_catalog_path
 
     # Generate all possible template combinations
-    templates = BaseConfig.generate_template_combinations()
-    template_names = [f"template_{i}" for i in range(len(templates))]
+    templates = BaseConfig.PromptOptions.generate_template_combinations()
+    template_names = list(templates.keys())
 
     # Get configurations
     datasets = BaseConfig.DatasetNames.get_datasets()
@@ -151,14 +216,14 @@ def run_experiment(local_catalog_path: str):
 
     # Generate experiments for each dataset type
     for dataset_name in datasets:
-        for prompt_template in BaseConfig.PromptParaphrases.get_all_prompt_paraphrases():
+        for prompt_paraphrase in BaseConfig.PromptParaphrases.get_all_prompt_paraphrases():
             for few_shots in BaseConfig.FewShot.get_values():
                 unitxt_recipe_args_by_groupings: Dict[str, List[UnitxtRecipeArgs]] = {
                     "Knowledge": [
                         _DefaultUnitxtRecipeArgs(
                             card=f"cards.{dataset_name}.{subset}",
                             template=[
-                                f"{prompt_template}.{template_name}"
+                                f"{prompt_paraphrase}.{template_name}"
                                 for template_name in template_names
                             ],
                             demos_pool_size=few_shots,
