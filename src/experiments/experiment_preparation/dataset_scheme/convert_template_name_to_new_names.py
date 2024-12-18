466fdacbd804dcab411b615c52bdae89e325bdc5
eliyahabba <eliyahabba@gmail.com>
2024-12-18 08:20:57 +0200

    add hg

--- src/experiments/experiment_preparation/dataset_scheme/convert_template_name_to_new_names.py
+++ src/experiments/experiment_preparation/dataset_scheme/convert_template_name_to_new_names.py
@@ -7,8 +7,12 @@ from typing import Dict, Tuple
 
 from tqdm import tqdm
 
+from config.get_config import Config
 from src.experiments.experiment_preparation.configuration_generation.ConfigParams import ConfigParams
+from src.experiments.experiment_preparation.dataset_scheme.JSON_schema_validator import validate_json_data
 from src.experiments.experiment_preparation.dataset_scheme.dataset_schema_adapter import convert_dataset
+from src.experiments.experiment_preparation.dataset_scheme.hf_dataset_manager.dataset_publisher import \
+    upload_to_huggingface
 from src.utils.Constants import Constants
 
 
@@ -46,6 +50,7 @@ def run_on_files(exp_dir: str) -> list[Path]:
                         for exp_file in exp_files:
                             exp_path = Path(exp_file)
                             mapping.append(exp_path)
+                            return mapping
                             break
 
     return mapping
@@ -58,13 +63,46 @@ def convert_to_scheme(item: Path, config_params, file_lock) -> None:
         with open(exp_file_path, 'r') as f:
             input_data = json.load(f)
         template_name=  input_data['template_name']
+        from src.utils.Constants import Constants
         catalog_path = Constants.TemplatesGeneratorConstants.CATALOG_PATH
-        template_path = os.path.join(catalog_path, template_name)
+        template_name.replace('MultipleChoiceTemplatesStructuredTopic', 'MultipleChoiceTemplatesStructuredWithTopic')
+        template_name.replace('MultipleChoiceTemplatesStructured', 'MultipleChoiceTemplatesStructuredWithoutTopic')
+        template_name = template_name.replace('MultipleChoiceTemplatesStructuredTopic', 'MultipleChoiceTemplatesStructuredWithTopic')
+        template_name = template_name.replace('MultipleChoiceTemplatesStructured', 'MultipleChoiceTemplatesStructuredWithoutTopic')
+
+        template_path = os.path.join(catalog_path, template_name + '.json')
         with open(template_path, 'r') as f:
             template_data = json.load(f)
 
-        converted_data = convert_dataset(input_data, template_data)
-        print(converted_data)
+        TemplatesGeneratorConstants = Constants.TemplatesGeneratorConstants
+        catalog_path = TemplatesGeneratorConstants.CATALOG_PATH
+        from src.experiments.data_loading.CatalogManager import CatalogManager
+        catalog_manager = CatalogManager(catalog_path)
+        template = catalog_manager.load_from_catalog(template_name)
+        from src.experiments.data_loading.DatasetLoader import DatasetLoader
+
+        llm_dataset_loader = DatasetLoader(card=input_data['card'],
+                                           template=template,
+                                           system_format=input_data['system_format'],
+                                           demos_taken_from='train' if input_data['num_demos']<1 else 'validation',
+                                           num_demos=input_data['num_demos'],
+                                           demos_pool_size=input_data['demos_pool_size'],
+                                           max_instances=input_data['max_instances'],
+                                           template_name=template_name)
+        llm_dataset = llm_dataset_loader.load()
+        dataset = llm_dataset.dataset
+        # Read the schema file
+        schema_file_path = r'/cs/labs/gabis/eliyahabba/LLM-Evaluation/src/experiments/experiment_preparation/dataset_scheme/scheme.json'
+        with open(schema_file_path, 'r') as file:
+            schema_data = json.load(file)
+        converted_data = convert_dataset(dataset, input_data, template_data)
+        validate_json_data(converted_data, schema_data)
+        REPO_NAME = "eliyahabba/llm-evaluation-dataset"  # Replace with your desired repo name
+        config = Config()
+        TOKEN = config.config_values.get("hf_access_token", "")
+
+        upload_to_huggingface(converted_data, repo_name=REPO_NAME, token=TOKEN, private=False)
+        # print(converted_data)
     except Exception as e:
         print(f"Error processing file {exp_file_path}: {str(e)}")
 
@@ -110,8 +148,8 @@ def main():
 
     print(f"\nFound {len(file_mapping)} matching file pairs")
     # split the file to 2 parts
-    file_mapping1 = {k: v for i, (k, v) in enumerate(file_mapping.items()) if i % 2 == 0}
-    file_mapping2 = {k: v for i, (k, v) in enumerate(file_mapping.items()) if i % 2 == 1}
+#    file_mapping1 = {k: v for i, (k, v) in enumerate(file_mapping.items()) if i % 2 == 0}
+#    file_mapping2 = {k: v for i, (k, v) in enumerate(file_mapping.items()) if i % 2 == 1}
     print(f"\nFound {len(file_mapping)} matching file pairs")
 
     print("\nRenaming files in parallel...")
@@ -122,3 +160,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
