22b102015e92d4acd7ad5a69ae0b4bc8b954df9a
ehabba <ehabba@lightricks.com>
2025-01-06 08:16:57 +0200

    updated prompts

--- src/experiments/experiment_preparation/datasets_configurations/HellaSwagConfig.py
+++ src/experiments/experiment_preparation/datasets_configurations/HellaSwagConfig.py
@@ -8,8 +8,8 @@ class HellaSwagConfig(BaseDatasetConfig):
     def get_hellaswag_instructions_standard(self) -> str:
         return f"Pick the best ending to the sentence.\nContext: {{context}}\nChoices:\n{{choices}}\nAnswer:"
 
-    def get_hellaswag_instructions_sentence(self) -> str:
-        return f"Pick the best ending to the Sentence.\nSentence: {{context}}\nChoices:\n{{choices}}\nAnswer:"
+    def get_hellaswag_instructions_context(self) -> str:
+        return f"Pick the best ending to the context.\nContext: {{context}}\nChoices:\n{{choices}}\nAnswer:"
 
     def get_hellaswag_instructions_structured(self) -> str:
         return f"Context: [context] Choices: [choices] Answer: [answer]\nContext: {{context}} Choices:\n{{choices}}\nAnswer:"
@@ -17,9 +17,29 @@ class HellaSwagConfig(BaseDatasetConfig):
     def get_hellaswag_instructions_basic(self) -> str:
         return f"Context: {{context}}\n\nChoices: {{choices}}\nAnswer:"
 
-    def get_hellaswag_paraphrase1(self) -> str:
+    def get_hellaswag_paraphrase_State_of_What_Art_paper_1(self) -> str:
         return f"Complete the following scenario by selecting the most appropriate ending.\n\nContext: {{context}}\n\nChoices:\n{{choices}}\nAnswer:"
 
+    def get_hellaswag_paraphrase_State_of_What_Art_paper_2(self) -> str:
+        return f"Select the most suitable conclusion for the sentence given the context: {{context}}. Here are your options: {{choices}}. Please provide your answer."
+
+    def get_hellaswag_paraphrase_State_of_What_Art_paper_3(self) -> str:
+        return f"Choose the most suitable ending for the sentence based on the given context.\nContext: {{context}}\nOptions:\n{{choices}}\nYour Answer:"
+
+    def get_hellaswag_paraphrase_State_of_What_Art_paper_4(self) -> str:
+        return f"Select the most suitable conclusion for the sentence.\nGiven Context: {{context}}\nOptions:\n{{choices}}\nResponse:"
+
+    def get_hellaswag_paraphrase_State_of_What_Art_paper_5(self) -> str:
+        return f"Choose the most suitable conclusion for the sentence.\nContext: {{context}}\nOptions:\n{{choices}}\nResponse:"
+
+    def get_hellaswag_paraphrase_State_of_What_Art_paper_6(self) -> str:
+        return f"Given the context and choices provided, select the most appropriate ending for the sentence. Use the context to understand the situation and choose the option that best completes the sentence. Context: {{context}} Choices: {{choices}} Answer:"
+
+    def get_hellaswag_paraphrase_State_of_What_Art_paper_7(self) -> str:
+        return f"Pick the best ending to the sentence based on the given context and choices. Use the context provided to determine the most suitable choice. \nContext: {{context}}\nChoices:\n{{choices}}\nAnswer:"
+
+    def get_hellaswag_paraphrase_State_of_What_Art_paper_8(self) -> str:
+        return f"Based on the provided context, choose the most suitable ending for the sentence from the given options. Context: {{context}} Choices: {{choices}} Answer:"
 
 if __name__ == "__main__":
     config = HellaSwagConfig({"shuffle_choices": True})
--- src/experiments/experiment_preparation/datasets_configurations/SocialQaConfig.py
+++ src/experiments/experiment_preparation/datasets_configurations/SocialQaConfig.py
@@ -1,39 +1,33 @@
 from src.experiments.experiment_preparation.datasets_configurations.BaseDatasetConfig import BaseDatasetConfig
 
 
-class MMLUConfig(BaseDatasetConfig):
+class SocialQaConfig(BaseDatasetConfig):
     def __init__(self, kwargs=None):
         super().__init__(kwargs)
 
     def get_mmlu_instructions_with_topic(self) -> str:
-        return f"The following are multiple choice questions (with answers) about {{topic}}.\n\n{{question}}\n{{choices}}\nAnswer:"
+        return f"The following are multiple choice questions (with answers) about common sense social interactions.\n\n{{question}}\n{{choices}}\nAnswer:"
 
     def get_mmlu_instructions_without_topic(self) -> str:
-        return f"The following are multiple choice questions (with answers).\n\n{{question}}\n\n{{choices}}\nAnswer:"
-
-    def get_mmlu_instructions_without_topic_fixed(self) -> str:
         return f"The following are multiple choice questions (with answers).\n\n{{question}}\n{{choices}}\nAnswer:"
 
     def get_mmlu_instructions_with_topic_helm(self) -> str:
-        return f"The following are multiple choice questions (with answers) about {{topic}}.\n\nQuestion: {{question}}\n{{choices}}\nAnswer:"
+        return f"The following are multiple choice questions (with answers) about common sense social interactions.\n\nQuestion: {{question}}\n{{choices}}\nAnswer:"
 
     def get_mmlu_instructions_without_topic_helm(self) -> str:
-        return f"The following are multiple choice questions (with answers).\n\nQuestion: {{question}}\n\n{{choices}}\nAnswer:"
-
-    def get_mmlu_instructions_without_topic_helm_fixed(self) -> str:
         return f"The following are multiple choice questions (with answers).\n\nQuestion: {{question}}\n{{choices}}\nAnswer:"
 
     def get_mmlu_instructions_without_topic_lm_evaluation_harness(self) -> str:
         return f"Question: {{question}}\n\nChoices: {{choices}}\nAnswer:"
 
     def get_structured_instruction_with_topic(self):
-        return f"Topic: {{topic}}\nQuestion: [question] Choices: [choices] Answer: [answer]\nQuestion: {{question}} Choices: {{choices}} Answer:"
+        return f"Topic: common sense social interactions\nQuestion: [question] Choices: [choices] Answer: [answer]\nQuestion: {{question}} Choices: {{choices}} Answer:"
 
     def get_structured_instruction_without_topic(self):
         return f"Question: [question] Choices: [choices] Answer: [answer]\nQuestion: {{question}} Choices: {{choices}} Answer:"
 
     def get_mmlu_instructions_with_topic_and_cot(self):
-        return (f"The following are multiple choice questions (with answers) about {{topic}}. Think step by"
+        return (f"The following are multiple choice questions (with answers) about common sense social interactions. Think step by"
                 f" step and then output the answer in the format of \"The answer is (X)\" at the end.\n\n")
 
     def get_please_simple_prompt_ProSA_paper(self) -> str:
@@ -46,16 +40,17 @@ class MMLUConfig(BaseDatasetConfig):
         return f"Could you provide a response to the following question:\n{{question}}\n{{choices}}\nAnswer:"
 
     def get_here_prompt_State_of_What_Art_paper(self) -> str:
-        return f"Here are some multiple choice questions along with their answers about {{topic}}.\n\nQuestion: {{question}}\nChoices: {{choices}}\nCorrect Answer:"
+        return f"Here are some multiple choice questions along with their answers about common sense social interactions.\n\nQuestion: {{question}}\nChoices: {{choices}}\nCorrect Answer:"
 
     def get_below_prompt_State_of_What_Art_paper(self) -> str:
-        return f"Below are multiple-choice questions related to {{topic}}, each followed by their respective answers.\n\nQuestion: {{question}}\nChoices: {{choices}}\nCorrect Answer:"
+        return f"Below are multiple-choice questions related to common sense social interactions, each followed by their respective answers.\n\nQuestion: {{question}}\nChoices: {{choices}}\nCorrect Answer:"
 
     def get_below_please_prompt_State_of_What_Art_paper(self) -> str:
-        return f"Below are multiple-choice questions related to {{topic}}. Please provide the correct answer for each question.\n\nQuestion: {{question}}\nChoices: {{choices}}\nAnswer:"
+        return f"Below are multiple-choice questions related to common sense social interactions. Please provide the correct answer for each question.\n\nQuestion: {{question}}\nChoices: {{choices}}\nAnswer:"
+
 
 
 if __name__ == "__main__":
-    config = MMLUConfig({"shuffle_choices": True})
+    config = SocialQaConfig({"shuffle_choices": True})
     config_dict = config.to_dict()
     print(config_dict)
