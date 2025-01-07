f69a9421b609c85a96a0e00328e78103aa244e17
ehabba <ehabba@lightricks.com>
2025-01-07 14:08:00 +0200

    fixed shuffle choices

--- src/experiments/experiment_preparation/configuration_generation/ConfigParams.py
+++ src/experiments/experiment_preparation/configuration_generation/ConfigParams.py
@@ -8,12 +8,26 @@ random.seed(42)
 class ConfigParams:
     GREEK_CHARS = "αβγδεζηθικ"  # 10 Greek letters
     KEYBOARD_CHARS = "!@#$%^₪*)("  # 26 lowercase letters
+    SHUFFLE_CHOICES_COMBINATIONS = {
+        "False": {"shuffle_choices": False, "sort_choices_by_length": False, "sort_choices_alphabetically": False,
+                    "reverse_choices": False, "place_correct_choice_position": None},
+        "lengthSort": {"shuffle_choices": False, "sort_choices_by_length": True, "sort_choices_alphabetically": False,
+                        "reverse_choices": False,"place_correct_choice_position": None},
+        "lengthSortReverse": {"shuffle_choices": False, "sort_choices_by_length": True,
+                                "sort_choices_alphabetically": False, "reverse_choices": True,"place_correct_choice_position": None},
+        "alphabeticalSort": {"shuffle_choices": False, "sort_choices_alphabetically": True,
+                              "sort_choices_by_length": False, "reverse_choices": False,"place_correct_choice_position": None},
+        "alphabeticalSortReverse": {"shuffle_choices": False, "sort_choices_alphabetically": True,
+                                      "sort_choices_by_length": False, "reverse_choices": True,"place_correct_choice_position": None},
+        "placeCorrectChoiceFirst": {"shuffle_choices": False, "sort_choices_alphabetically": False,
+                                "sort_choices_by_length": False, "reverse_choices": False,"place_correct_choice_position": 0},
+    }
+
     override_options = {
         "enumerator": ["capitals", "lowercase", "numbers", "roman", KEYBOARD_CHARS, GREEK_CHARS],
 
         "choices_separator": [" ", "\n", ", ", "; ", " | ", " OR ", " or "],
-        "shuffle_choices": [False, True],
-        # Add more parameters and their possible values as needed
+        "shuffle_choices": list(SHUFFLE_CHOICES_COMBINATIONS.keys()),
     }
 
     ENUM_CHARS = {"ABCDEFGHIJKLMNOP": "capitals",
@@ -72,6 +86,10 @@ class ConfigParams:
             camel_key = cls.to_camel_case(key)
             parts.append(f"{camel_key}_{formatted_value}")
         return "_".join(parts)
+
+    @staticmethod
+    def get_shuffle_choices_argument(shuffle_choices_name:str) -> Dict[str, bool]:
+        return ConfigParams.SHUFFLE_CHOICES_COMBINATIONS[shuffle_choices_name]
 
     @staticmethod
     def to_camel_case(snake_str: str) -> str:
