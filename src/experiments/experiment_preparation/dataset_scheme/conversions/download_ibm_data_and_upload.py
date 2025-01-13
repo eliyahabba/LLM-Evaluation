469eaaa2db4d6b74685e87a303a1067612b28fc9
ehabba <ehabba@lightricks.com>
2025-01-13 17:50:14 +0200

    save the data at tmp path

--- src/experiments/experiment_preparation/dataset_scheme/conversions/download_ibm_data_and_upload.py
+++ src/experiments/experiment_preparation/dataset_scheme/conversions/download_ibm_data_and_upload.py
@@ -20,18 +20,11 @@ if __name__ == "__main__":
 
     # List of URLs to download
     args.urls = [
-        # "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-16T00%3A00%3A00%2B00%3A00_2024-12-17T00%3A00%3A00%2B00%3A00.parquet",
-        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-17T00%3A00%3A00%2B00%3A00_2024-12-18T00%3A00%3A00%2B00%3A00.parquet",
-        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-18T00%3A00%3A00%2B00%3A00_2024-12-19T00%3A00%3A00%2B00%3A00.parquet",
-        # "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-19T00%3A00%3A00%2B00%3A00_2024-12-20T00%3A00%3A00%2B00%3A00.parquet",
-        # "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-20T00%3A00%3A00%2B00%3A00_2024-12-21T00%3A00%3A00%2B00%3A00.parquet",
-        # "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-21T00%3A00%3A00%2B00%3A00_2024-12-22T00%3A00%3A00%2B00%3A00.parquet"
-        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-22T08%3A00%3A00%2B00%3A00_2024-12-22T12%3A00%3A00%2B00%3A00.parquet",
-        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-22T16%3A00%3A00%2B00%3A00_2024-12-22T18%3A00%3A00%2B00%3A00.parquet",
-        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-22T18%3A00%3A00%2B00%3A00_2024-12-22T20%3A00%3A00%2B00%3A00.parquet",
-        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-22T20%3A00%3A00%2B00%3A00_2024-12-22T22%3A00%3A00%2B00%3A00.parquet",
-        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-22T22%3A00%3A00%2B00%3A00_2024-12-23T00%3A00%3A00%2B00%3A00.parquet",
-        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2024-12-22T22%3A00%3A00%2B00%3A00_2024-12-23T00%3A00%3A00%2B00%3A00.parquet",
+        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2025-01-09T19%3A00%3A00%2B00%3A00_2025-01-09T23%3A00%3A00%2B00%3A00.parquet",
+        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2025-01-10T14%3A00%3A00%2B00%3A00_2025-01-11T18%3A00%3A00%2B00%3A00.parquet",
+        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2025-01-10T00%3A00%3A00%2B00%3A00_2025-01-10T10%3A00%3A00%2B00%3A00.parquet",
+        "https://huggingface.co/datasets/OfirArviv/HujiCollabOutput/resolve/main/data_2025-01-10T11%3A00%3A00%2B00%3A00_2025-01-10T13%3A00%3A00%2B00%3A00.parquet",
     ]
+
     download_huggingface_files_parllel(output_dir=Path(args.output_dir), urls=args.urls, repo_name=args.repo_name,
                                        scheme_files_dir=args.scheme_files_dir, probs=args.probs)
