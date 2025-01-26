de3d3c30708bca3229f4e030be16ab2ad221f0e1
ehabba <ehabba@lightricks.com>
2025-01-26 13:47:21 +0200

    updated data

---
+++ src/analysis/create_plots/main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --mem=100g
+#SBATCH -c24
+#SBATCH --time=4:0:0
+
+#SBATCH --mail-user=eliya.habba@mail.huji.ac.il
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+
+
+# Now HF_HOME is available to use in this script
+echo "HF_HOME is set to: $HF_HOME"
+export PYTHONPATH=/cs/labs/gabis/eliyahabba/LLM-Evaluation/
+
+sacct -j $SLURM_JOB_ID --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,nodelist
+dir="/cs/labs/gabis/eliyahabba/LLM-Evaluation/src/analysis/create_data"
+absolute_path=$(readlink -f $dir)
+# print the full (not relative) path of the dir variable
+echo "current dir is set to: $absolute_path"
+cd $dir
+
+echo ${SLURM_ARRAY_TASK_ID}
+export UNITXT_ALLOW_UNVERIFIED_CODE="True"
+CUDA_LAUNCH_BLOCKING=1 python main.py
