00adb32bf2ff1f32e2a3d5d8d46ca9369266dc3b
ehabba <ehabba@lightricks.com>
2025-01-27 22:45:54 +0200

    updated data

--- src/analysis/create_data/save_data_for_analysis.py
+++ src/analysis/create_data/save_data_for_analysis.py
@@ -443,6 +443,7 @@ def download_huggingface_files_parllel(input_dir: Path, process_input_dir, file_
     os.makedirs(input_dir, exist_ok=True)
 
     num_processes = min(24, cpu_count() - 1)
+    num_processes = 12
     logger = setup_logging()
     logger.info("Starting processing...")
     logger.info(f"Starting parallel processing with {num_processes} processes...")
@@ -496,8 +497,8 @@ if __name__ == "__main__":
                         default="eliyahabba/llm-evaluation-analysis")
     parser.add_argument('--scheme_files_dir', help='Directory to store the scheme files')
     args = parser.parse_args()
-    args.old_input_dir = "/cs/snapless/gabis/eliyahabba/ibm_results_data_full"
-    args.input_dir = "/cs/labs/gabis/eliyahabba/ibm_results_data_full"
+    args.input_dir = "/cs/snapless/gabis/eliyahabba/ibm_results_data_full"
+    # args.input_dir = "/cs/labs/gabis/eliyahabba/ibm_results_data_full"
 
     process_input_dir = "/cs/snapless/gabis/eliyahabba/ibm_results_data_full_processed"
     if not os.path.exists(process_input_dir):
