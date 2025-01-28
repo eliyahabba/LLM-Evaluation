dfb0a7a3b5f74d9a80c984065df4e9d46d1cf0b4
ehabba <ehabba@lightricks.com>
2025-01-28 22:54:46 +0200

    updated data

--- src/analysis/create_data/upload_data_for_analysis_split.py
+++ src/analysis/create_data/upload_data_for_analysis_split.py
@@ -57,8 +57,11 @@ def download_huggingface_files_parllel(process_output_dir):
     logger = setup_logging()
     logger.info("Starting processing...")
     logger.info(f"Starting parallel processing with {num_processes} processes...")
-    files = os.listdir(process_output_dir)
-    files = [os.path.join(process_output_dir, file) for file in files]
+
+    sub_folderfiles = [folder for folder in os.listdir(process_output_dir) if os.path.isdir(os.path.join(process_output_dir, folder))]
+    files = []
+    for folder in sub_folderfiles:
+        files.extend([os.path.join(folder, file) for file in os.listdir(os.path.join(process_output_dir, folder))])
     print(f"Processing {len(files)} files")
     with Manager() as manager:
         process_func = partial(process_file_safe, logger=logger)
