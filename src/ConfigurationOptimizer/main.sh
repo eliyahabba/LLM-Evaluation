636b504931e56a8dcc2c58836c1bacfdbf969b36
ehabba <ehabba@lightricks.com>
2025-02-07 10:43:48 +0200

    added bash file

---
+++ src/ConfigurationOptimizer/main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --mem-per-cpu=20g
+#SBATCH -c 10
+#SBATCH --time=1:0:0
+
+#SBATCH --mail-user=eliya.habba@mail.huji.ac.il
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+#SBATCH --exclude=gringolet-01,gringolet-02,gringolet-03,gringolet-04,gringolet-05,gringolet-06
+
+
+export PYTHONPATH=/cs/labs/gabis/eliyahabba/LLM-Evaluation/
+sacct -j $SLURM_JOB_ID --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,nodelist
+dir="/cs/labs/gabis/eliyahabba/LLM-Evaluation/src/ConfigurationOptimizer
+absolute_path=$(readlink -f $dir)
+# print the full (not relative) path of the dir variable
+echo "current dir is set to: $absolute_path"
+cd $dir
+
+echo ${SLURM_ARRAY_TASK_ID}
+export UNITXT_ALLOW_UNVERIFIED_CODE="True"
+CUDA_LAUNCH_BLOCKING=1 python main.py;
