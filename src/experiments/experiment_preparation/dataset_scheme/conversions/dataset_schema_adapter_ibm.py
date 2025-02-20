3d2b4285960277465acb25d2a5cfc8bc792bb053
ehabba <ehabba@lightricks.com>
2025-02-20 16:09:57 +0200

    added logs

--- src/experiments/experiment_preparation/dataset_scheme/conversions/dataset_schema_adapter_ibm.py
+++ src/experiments/experiment_preparation/dataset_scheme/conversions/dataset_schema_adapter_ibm.py
@@ -210,8 +210,8 @@ class SchemaConverter:
             if isinstance(task_data, dict):
                 # take the 0 key
                 # assert there is only 1 key
-                assert len(task_data) == 1
-                task_data = task_data[list(task_data.keys())[0]]
+                if len(task_data) == 1:
+                    task_data = task_data[list(task_data.keys())[0]]
 
             
             recipe = self._parse_config_string(row['run_unitxt_recipe'])
