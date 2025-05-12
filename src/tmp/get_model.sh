cdae4a5b29bbe1a1f0e5f1f0c4cdc434356a6010
ehabba <ehabba@lightricks.com>
2025-05-12 10:26:58 +0300

    ImproveRobustness

--- src/tmp/get_model.sh
+++ src/tmp/get_model.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-#SBATCH --mem=10g
+#SBATCH --mem=200g
 #SBATCH --time=2:0:0
 #SBATCH --mail-user=eliya.habba@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --job-name=calculate_perplexity
 #SBATCH --killable
 
+export HF_HOME=/cs/snapless/gabis/gabis/shared/huggingface/
 
 # Now HF_HOME is available to use in this script
 
-dir="/Users/ehabba/PycharmProjects/LLM-Evaluation/src/tmp/"
-absolute_path=$(readlink -f $dir)
+#dir="/Users/ehabba/PycharmProjects/LLM-Evaluation/src/tmp/"
+#absolute_path=$(readlink -f $dir)
 # print the full (not relative) path of the dir variable
-echo "current dir is set to: $absolute_path"
-cd $dir
+#echo "current dir is set to: $absolute_path"
+#cd $dir
 
-PROJECT_DIR="/cs/snapless/gabis/eliyahabba/LLM-Evaluation/src/tmp/"
+PROJECT_DIR="/cs/labs/gabis/eliyahabba/LLM-Evaluation/src/tmp/"
 cd $PROJECT_DIR
 echo "Current directory: $(pwd)"
 
-source "/cs/snapless/gabis/eliyahabba/venvs/LLM-Evaluation/bin/activate"
-echo "Virtual environment activated"
+#source "/cs/snapless/gabis/eliyahabba/venvs/LLM-Evaluation/bin/activate"
+#echo "Virtual environment activated"
 
 sacct -j $SLURM_JOB_ID --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,nodelist
 module load cuda
